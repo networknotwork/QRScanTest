4b0e142feeff8a208ee7c6b5d3e9f268261dd16f
Rusty Boot <nicholastuso@yahoo.com>
2019-08-10 10:24:22 -0700

    Updates

---
+++ precache-manifest.02dfc16151c313bb1c33f05e58eb2ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb63af16d9cdfce110374a76de6e9e8",
+    "url": "/QRScanTest/index.html"
+  },
+  {
+    "revision": "00e3ee762bc1ba7ee47a",
+    "url": "/QRScanTest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "305c3d09072176c18c57",
+    "url": "/QRScanTest/static/js/2.ac7ef5cc.chunk.js"
+  },
+  {
+    "revision": "00e3ee762bc1ba7ee47a",
+    "url": "/QRScanTest/static/js/main.7b536751.chunk.js"
+  },
+  {
+    "revision": "8bf28b830f71253ac92e",
+    "url": "/QRScanTest/static/js/runtime~main.4c306b44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QRScanTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
